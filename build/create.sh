08de52bee4328dc3b0265dbe99fdf47b16e755ef
Cesar <cesar.valdez@agriculture.gov.ie>
2019-10-16 13:39:18 +0100

    merging

--- build/create.sh
+++ build/create.sh
@@ -1,2 +1,3 @@
 oc new-build -n cicd --name sso-bot --strategy=docker --binary=true mhart/alpine-node
-oc create -f build\agent.yml -n cicd -f
+oc create dc sso-bot --image=docker-registry.default.svc:5000/cicd/sso-bot
+oc set triggers dc/sso-bot --from-image=is/sso-bot:latest -c default-container
