1f37d63fe61c1b221027da21a3a0b28e97d67f3c
Cesar Valdez <cesar.valdez@agriculture.gov.ie>
2019-11-20 14:54:48 +0000

    adding new functionalities

---
+++ build.sh
@@ -0,0 +1,1 @@
+ oc start-build -n cicd bc/sso-bot --from-file=. --follow
--- sso.js
+++ sso.js
@@ -1,11 +1,44 @@
 const CMD = require('./lib/cmd')
 const Clean = require('./lib/clean')
 const OAuth2 = require('./lib/oauth2')
-const KeycloakConfigurator = require('./lib/configure')
 const ssecure_service = require('./lib/secure_service_tester')
 const SPI = require('./lib/spi')
-const CreateOKDProject = require('./lib/ocp/create')
 const _ = require('lodash')
+
+
+const CreateOKDProject = require('./lib/ocp/create')
+const OKDResourceTemplate = require('./lib/ocp/template')
+const ImageBuilder = require('./lib/ocp/image-builder')
+
+
+
+let user     = process.env['RHSSO_ADMIN_USER'] || 'admin'
+let password = process.env['RHSSO_ADMIN_PASSWORD'] || 'admin'
+
+
+async function KCResourceFactory(resource, url, realm, id){
+
+  if(_.isEmpty(url)) {
+    console.log(`Need an URL!!`)
+    process.exit(-1)
+  }
+
+  if(_.isEmpty(resource)){
+    console.log(`Need an resource example: node sso.js -get url realms`)
+    process.exit(-1)
+  }
+
+  
+  let spi = await SPI(url, user, password, realm, id)
+  let spiResource = spi[resource]
+
+  if(_.isEmpty(spiResource)){
+    console.log(`Resource (${spiResource}) doesn't exist or not implemented`)
+    process.exit(-1)
+  }
+
+  return spiResource
+}
 
 /*
   Usage: node sso.js -param args
@@ -18,9 +51,9 @@ const _ = require('lodash')
 
 new CMD({
   clean: () => new Clean().doIt(),
-  
+
   clear: () => new Clean().doIt(),
-  
+
   url: (url, name, password, realm) => {
     console.log(' == calling:', url, 'name:', name, 'password:', password)
     OAuth2.loginWithCredentials(url, name, password, realm)
@@ -28,6 +61,7 @@ new CMD({
 
   impersonate: (url, name) => {
     console.log(' == calling:', url, 'name:', name)
+
     OAuth2.loginAsDevUser(url, name)
   },
 
@@ -41,74 +75,44 @@ new CMD({
     ssecure_service(url, name, svc)
   },
 
- 'get': async function(resource, url, realm) {
-    let user     = process.env['RHSSO_ADMIN_USER'] || 'admin'
-    let password = process.env['RHSSO_ADMIN_PASSWORD'] || 'admin'
-    
-    if(_.isEmpty(url)){
-      console.log(`Need an URL!!`)
-      process.exit(-1)
-    }
+  get: async function(resource, url, realm, id) {
 
-    if(_.isEmpty(resource)){
-      console.log(`Need an resource example: node sso.js -get url realms`)
-      process.exit(-1)
-    }
-  
-    let spi = await SPI(url, user, password, realm)
-    let spiResource = spi[resource]
+    let keycloakREST = await KCResourceFactory(resource, url, realm, id)
 
-    if(_.isEmpty(spiResource)){
-      console.log(`Resource (${spiResource}) doesn't exist or not implemented`)
-      process.exit(-1)
-    }
-
-    let federated = await spiResource.get()
+    let federated = await keycloakREST.get()
     console.log(federated)
   },
 
+  post: async function(resource, url, file, realm, id) {
 
- 'post': async function(resource, url, file, realm) {
-    let user     = process.env['RHSSO_ADMIN_USER'] || 'admin'
-    let password = process.env['RHSSO_ADMIN_PASSWORD'] || 'admin'
-    
-    if(_.isEmpty(url)){
-      console.log(`Need an URL!!`)
-      process.exit(-1)
-    }
-
-    if(_.isEmpty(resource)){
-      console.log(`Need an resource example: node sso.js -get url realms`)
-      process.exit(-1)
-    }
-
+    let keycloakREST = await KCResourceFactory(resource, url, realm, id)
     let payload = JSON.parse( require('fs').readFileSync(file).toString() ) 
+    
     console.log('payload=>', payload)
     
-    let spi = await SPI(url, user, password, realm)
-    let spiResource = spi[resource]
-
-    if(_.isEmpty(spiResource)){
-      console.log(`Resource (${spiResource}) doesn't exist or not implemented`)
-      process.exit(-1)
-    }
-
-    let federated = await spiResource.post(payload)
+    let federated = await keycloakREST.post(payload)
     console.log(federated)
   },
-
    
-  roles: (url, username, password, realm) => {
-      KeycloakConfigurator.run(url, username, password, realm)
-  },
-
-  config: (url, username, password, realm) => {
-      KeycloakConfigurator.run(url, username, password, realm)
-  },
-
   install: (name, project, token) => {
     console.log('installing that -> ', name, project, token)
-    CreateOKDProject(name, project, token)
+    let simpleSSO  = new OKDResourceTemplate('./lib/ocp/templates/sso.json')
+    let okdCreator = new CreateOKDProject(simpleSSO)
+
+    okdCreator.create(name, project, token)
+  },
+
+  builder: (name, project, token) => {
+
+    ImageBuilder(name, project, token)
+  },
+
+  "install-with-pg": (name, project, token) => {
+    console.log('installing with-pg that -> ', name, project, token)
+    let postgressSSO = new OKDResourceTemplate('./lib/ocp/templates/sso-postgress.json')
+    let okdCreator   = new CreateOKDProject(postgressSSO)
+
+    okdCreator.create(name, project, token)
   }
 
 }).run()
